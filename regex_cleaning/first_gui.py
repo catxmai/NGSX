d8f53783a788f3bf41dd01eec22d2a25bfc9c1a9
catxmai <catmaimx@gmail.com>
2020-04-20 13:05:26 -0400

    added some buttons

--- regex_cleaning/first_gui.py
+++ regex_cleaning/first_gui.py
@@ -1,28 +1,53 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
-
-
-
-# In[9]:
-
-
 import tkinter as tk
+import pandas as pd
+import csv
+from tkinter import *
+from tkinter import filedialog as fd
 
-root= tk.Tk()
 
-canvas1 = tk.Canvas(root, width = 300, height = 300)
-canvas1.pack()
+def import_csv():
+    global file_name
+    global df
+    csv_file_path = fd.askopenfile(filetypes=[("csv files", "*.csv")])
+    file_name.set(csv_file_path)
+    df = pd.read_csv(csv_file_path)
+    print(df.head())
 
-def hello ():  
-    label1 = tk.Label(root, text= 'Hello World!', fg='green', font=('helvetica', 12, 'bold'))
-    canvas1.create_window(150, 200, window=label1)
-    
-button1 = tk.Button(text='Click Me',command=hello, bg='brown',fg='white')
-canvas1.create_window(150, 150, window=button1)
+
+def write_csv():
+    files = [('All Files', '*.*'),
+             ('csv Files', '*.csv'),
+             ('Text Document', '*.txt')]
+    mylist = [1,2,3]
+    fp = fd.asksaveasfile(mode='w', filetypes=files, defaultextension=".csv")
+    if fp is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    csv_writer = csv.writer(fp, delimiter=",",
+                            quotechar="'",
+                            quoting=csv.QUOTE_MINIMAL,
+                            lineterminator="\n")
+    csv_writer.writerow(mylist)
+
+
+root = tk.Tk()
+root.title("NGSX Clean Transcript")
+w = 800  # width for the Tk root
+h = 650  # height for the Tk root
+
+ws = root.winfo_screenwidth()  # width of the screen
+hs = root.winfo_screenheight()  # height of the screen
+
+# calculate x and y coordinates for the Tk root window
+x = (ws / 2) - (w / 2)
+y = (hs / 2) - (h / 2)
+
+# set the dimensions of the screen
+# and where it is placed
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+tk.Button(root, text='Browse .csv File', command=import_csv).grid(row=1, column=0)
+file_name = tk.StringVar()
+tk.Label(root, textvariable=file_name).grid(row=0, column=1)
+tk.Button(root, text='Close', command=root.destroy).grid(row=1, column=2)
+tk.Button(root, text='Save as', command=write_csv).grid(row=1, column=3)
 
 root.mainloop()
-
