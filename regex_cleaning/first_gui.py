954e859be01dd8695eafb5bfcc86ced4d40391e9
catxmai <catmaimx@gmail.com>
2020-04-20 21:02:04 -0400

    built executable, first_gui.exe finally runs!

--- regex_cleaning/first_gui.py
+++ regex_cleaning/first_gui.py
@@ -1,53 +1,95 @@
-import tkinter as tk
 import pandas as pd
+# import pywintypes
+# import win32api
+import tkinter
 import csv
+import re
 from tkinter import *
-from tkinter import filedialog as fd
+from tkinter import filedialog
+import os
+
+global file_name
+global df
 
 
 def import_csv():
-    global file_name
     global df
-    csv_file_path = fd.askopenfile(filetypes=[("csv files", "*.csv")])
-    file_name.set(csv_file_path)
-    df = pd.read_csv(csv_file_path)
-    print(df.head())
+    global file_name
 
-
-def write_csv():
-    files = [('All Files', '*.*'),
-             ('csv Files', '*.csv'),
-             ('Text Document', '*.txt')]
-    mylist = [1,2,3]
-    fp = fd.asksaveasfile(mode='w', filetypes=files, defaultextension=".csv")
-    if fp is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+    files = [('All Files', '*.*'),('csv Files', '*.csv'), ('Excel Files', '*.xlsx')]
+    csv_file_path = filedialog.askopenfile(filetypes=files)
+    if csv_file_path is None:
         return
-    csv_writer = csv.writer(fp, delimiter=",",
-                            quotechar="'",
-                            quoting=csv.QUOTE_MINIMAL,
-                            lineterminator="\n")
-    csv_writer.writerow(mylist)
+    file_name.set(csv_file_path.name)
+    name = os.path.basename(csv_file_path.name)
+    if name.endswith('.csv'):
+        df = pd.read_csv(csv_file_path, encoding="utf-8", header=None)
+    if name.endswith('.xlsx'):
+        xl = pd.ExcelFile(name)
+        df = xl.parse("Sheet1", index_col=None, header=None)
+        # df = pd.read_excel(csv_file_path, 'Sheet1', encoding="cp1252", index_col=None)
+    print(df.head())
+    df.columns = ["timestamp", "name", "text"]
+    for index, col in df.iterrows():
+        if re.search(r"ï»¿", str(col["timestamp"])):  # not sure how to fix encoding utf-8 here
+            text = re.sub(r"ï»¿", "", str(col["timestamp"]))
+            df.loc[index]["timestamp"] = text
+            break
+    # print(df.head())
 
 
-root = tk.Tk()
+def write_excel():
+    files = [('All Files', '*.*'),
+             ('Excel Files', '*.xlsx')]
+    excel_save = filedialog.asksaveasfile(mode='w', filetypes=files, defaultextension=".xlsx")
+    if excel_save is None:  # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    print(excel_save.name)
+    df.to_excel(excel_save.name, header=False, index=False)
+
+
+def clean_text(command):
+    global df
+    regex = ""
+
+    if command == "square_bracket":
+        regex = "(\[.*?\])"
+    if command == "um":
+        regex = "\s(um)\s*?|\s*?(um)\s|\s(uh)\s*?|\s*?(uh)\s|\s(ah)\s*?|\s*?(ah)\s"
+    texts = []
+    for index, col in df.iterrows():
+        text = re.sub(regex, "", col["text"])
+        texts.append(text)
+    df["cleaned text"] = texts
+    print(df["cleaned text"])
+
+
+root = tkinter.Tk()
 root.title("NGSX Clean Transcript")
-w = 800  # width for the Tk root
-h = 650  # height for the Tk root
+w = 600  # width for the tkinter root
+h = 300  # height for the tkinter root
 
 ws = root.winfo_screenwidth()  # width of the screen
 hs = root.winfo_screenheight()  # height of the screen
 
-# calculate x and y coordinates for the Tk root window
-x = (ws / 2) - (w / 2)
-y = (hs / 2) - (h / 2)
+# calculate x and y coordinates for the tkinter root window
+x = (ws / 4) - (w / 4)
+y = (hs / 4) - (h / 4)
 
 # set the dimensions of the screen
 # and where it is placed
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-tk.Button(root, text='Browse .csv File', command=import_csv).grid(row=1, column=0)
-file_name = tk.StringVar()
-tk.Label(root, textvariable=file_name).grid(row=0, column=1)
-tk.Button(root, text='Close', command=root.destroy).grid(row=1, column=2)
-tk.Button(root, text='Save as', command=write_csv).grid(row=1, column=3)
+tkinter.Button(root, text='Browse .csv or Excel File', command=import_csv).grid(sticky="w",row=1, column=0)
+file_name = tkinter.StringVar()
+tkinter.Label(root, textvariable=file_name,wraplength=400, justify=LEFT, anchor="w").grid(row=0, column=3)
+tkinter.Button(root, text='Clean all square brackets', command=lambda:clean_text("square_bracket"), anchor="w").grid(sticky="w",row=2, column=0)
+tkinter.Button(root, text='Clean all standalone um ah uh', command=lambda:clean_text("um"), anchor="w").grid(sticky="w",row=2, column=1)
+tkinter.Button(root, text='Save as', command=write_excel,anchor="w").grid(sticky="w",row=3, column=0)
+tkinter.Button(root, text='Close', command=root.destroy,anchor="w").grid(sticky="w",row=4, column=0)
+
+bottom = tkinter.Frame(root)
+bottom_label = tkinter.Label(bottom, text="Feature requests to CMai@clarku.edu")
+bottom_label.pack(side="bottom", fill="x")
+bottom.grid(pady=y-30)
 
 root.mainloop()
--- regex_cleaning/first_gui.spec
+++ regex_cleaning/first_gui.spec
@@ -1,33 +1,44 @@
-# -*- mode: python ; coding: utf-8 -*-
-
+# -*- mode: python ; coding: utf-8 -*
 block_cipher = None
 
+def get_pandas_path():
+    import pandas
+    pandas_path = pandas.__path__[0]
+    return pandas_path
 
 a = Analysis(['first_gui.py'],
              pathex=['C:\\Users\\Cat Mai\\Documents\\Work\\NGSX\\Coding\\NGSX\\regex_cleaning'],
              binaries=[],
              datas=[],
              hiddenimports=[],
-             hookspath=[],
-             runtime_hooks=[],
+             hookspath=None,
+             runtime_hooks=None,
              excludes=[],
              win_no_prefer_redirects=False,
              win_private_assemblies=False,
-             cipher=block_cipher,
-             noarchive=False)
+             cipher=block_cipher)
+
+dict_tree = Tree(get_pandas_path(), prefix='pandas', excludes=["*.pyc"])
+a.datas += dict_tree
+a.binaries = filter(lambda x: 'pandas' not in x[0], a.binaries)
+
 pyz = PYZ(a.pure, a.zipped_data,
              cipher=block_cipher)
 exe = EXE(pyz,
           a.scripts,
-          a.binaries,
-          a.zipfiles,
-          a.datas,
           [],
+          exclude_binaries=True,
           name='first_gui',
           debug=False,
           bootloader_ignore_signals=False,
           strip=False,
           upx=True,
-          upx_exclude=[],
-          runtime_tmpdir=None,
           console=False )
+scoll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=False,
+               upx_exclude=[],
+               name='first_gui')
